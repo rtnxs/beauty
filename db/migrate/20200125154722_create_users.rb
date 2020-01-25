128a7604546b6da2d7c29f6a3924e8ad67a1cb03
rtnxs <riotnexus@mail.ru>
2020-01-25 19:12:50 +0300

    added devise user

--- db/migrate/20200125154722_create_users.rb
+++ db/migrate/20200125154722_create_users.rb
@@ -1,8 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string :email
-      t.string :password
+      t.string :name
       t.boolean :admin, null: false, default: false
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_25_154722) do
+ActiveRecord::Schema.define(version: 2020_01_25_160633) do
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password"
+    t.string "name"
     t.boolean "admin", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
